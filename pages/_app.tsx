0408ea6889ceb4957ef36d8a36b1017e9830f10f
Joaquin Blanco <blancojoaquin77@gmail.com>
2022-10-20 04:15:50 -0300

    update tsconfig and paths

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,10 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import type { AppProps } from "next/app";
-import Layout from "../src/layout/Layout";
+import Layout from "@/src/layout/Layout";
 import { CssBaseline } from "@mui/material";
 import { Provider } from "react-redux";
-import { store } from "../src/store/store";
+import { store } from "@/src/store/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from "next";
-import Table from "../src/admin/Table";
+import Table from "@/src/admin/Table";
 import axios from "axios";
-import { Admin } from "../src/admin/sliceAdmin";
-import { useAppDispatch } from "../src/store/hooks";
-import { setAdmins } from "../src/admin/sliceAdmin";
+import { Admin } from "@/src/admin/sliceAdmin";
+import { useAppDispatch } from "@/src/store/hooks";
+import { setAdmins } from "@/src/admin/sliceAdmin";
 
 interface PropsNextPage {
   apiurl: string;
@@ -11,16 +11,13 @@ interface PropsNextPage {
 }
 
 const Index: NextPage<PropsNextPage> = ({ apiurl, admins }) => {
-  console.log(apiurl);
   const dispatch = useAppDispatch();
   dispatch(setAdmins(admins));
   return <Table />;
 };
 
 export const getServerSideProps = async () => {
-  const apiurl = process.env.APIURL ?? "";
-  console.log(apiurl);
-
+  const apiurl = process.env.APIURL ?? "http://localhost:3000/api";
   const res = await axios.get(`${apiurl}/administradores`);
   const admins = res.status === 200 ? res.data.admins : [];
   return {
