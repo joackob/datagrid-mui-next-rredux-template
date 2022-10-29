43ecb932087507cb3349cba4170b3f2d3874a676
Joaquin Blanco <blancojoaquin77@gmail.com>
2022-10-29 01:05:23 -0300

    Update AddButton.tsx

--- src/admins/components/AddButton.tsx
+++ src/admins/components/AddButton.tsx
@@ -1,30 +1,113 @@
+import { ChangeEvent, FormEvent, MouseEvent } from "react";
 import { Add } from "@mui/icons-material";
-import { Button } from "@mui/material";
+import { Box, Button, Card, Popper, TextField } from "@mui/material";
 import { useAppDispatch } from "@/store/hooks";
 import { add } from "../thunks";
+import { Fragment, useState } from "react";
+import { AdminProps } from "../interfaces";
 
 const AddButton = () => {
   const dispatch = useAppDispatch();
+  const [anchor, setAnchor] = useState<null | HTMLElement>(null);
+  const handleClick = (event: MouseEvent<HTMLElement>) => {
+    setAnchor(anchor ? null : event.currentTarget);
+  };
+  const open = Boolean(anchor);
+  const id = open ? "popper-id" : undefined;
 
-  const handleClick = () => {
-    const nombre = prompt("nombre?");
-    if (!nombre) return;
-    const apellido = prompt("apellido?");
-    if (!apellido) return;
-    const email = prompt("email?");
-    if (!email) return;
+  const [adminProps, setAdminProps] = useState<AdminProps>({
+    nombre: "",
+    apellido: "",
+    email: "",
+  });
 
-    dispatch(add({ nombre, apellido, email }));
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setAdminProps({
+      ...adminProps,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    dispatch(add(adminProps));
+    setAnchor(null);
   };
 
   return (
-    <Button
-      startIcon={<Add />}
-      className="button button-text"
-      onClick={handleClick}
-    >
-      add
-    </Button>
+    <Fragment>
+      <Button
+        startIcon={<Add />}
+        className="button button-text"
+        aria-describedby={id}
+        onClick={handleClick}
+      >
+        add
+      </Button>
+      <Popper open={open} anchorEl={anchor} id={id} placement="bottom-start">
+        <Card variant="outlined">
+          <Box
+            component="form"
+            p={2}
+            sx={{
+              backgroundColor: "white",
+            }}
+            onSubmit={handleSubmit}
+          >
+            <Box
+              display={"flex"}
+              flexDirection={{
+                xs: "column",
+                md: "row",
+              }}
+              mb={2}
+              width={{
+                xs: "250px",
+                md: "650px",
+              }}
+              justifyContent="space-between"
+            >
+              <TextField
+                margin="dense"
+                size="small"
+                label="Nombre"
+                name="nombre"
+                required
+                type={"text"}
+                onChange={handleChange}
+              />
+              <TextField
+                margin="dense"
+                size="small"
+                label="Apellido"
+                name="apellido"
+                required
+                type={"text"}
+                onChange={handleChange}
+              />
+              <TextField
+                margin="dense"
+                size="small"
+                label="Correo electronico"
+                name="email"
+                required
+                type={"email"}
+                onChange={handleChange}
+              />
+            </Box>
+            <Button
+              type="submit"
+              variant="contained"
+              size="small"
+              className="button contained"
+            >
+              ADD
+            </Button>
+          </Box>
+        </Card>
+      </Popper>
+    </Fragment>
   );
 };
 
